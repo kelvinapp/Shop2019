87d25ee953a6ade200a4dc503d536efb63ca96b1
kelvinapp <kelvinapp2017@gmail.com>
2019-10-16 16:29:22 +0800

    5

--- app/src/main/java/com/kelvin/shop2019/SinginActivity.kt
+++ app/src/main/java/com/kelvin/shop2019/SinginActivity.kt
@@ -4,22 +4,64 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_singin.*
 
 class SinginActivity : AppCompatActivity() {
+    private val RC_GOOGLE_SING_IN = 200
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private  val TAG = SinginActivity::class.java.simpleName
 
     private val RC_TEST =500
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_singin)
+
+        val gso =GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        google_sign_in.setOnClickListener {
+            startActivityForResult(googleSignInClient.signInIntent,RC_GOOGLE_SING_IN)
+        }
+
         signUp.setOnClickListener {
             signUp()
         }
         login.setOnClickListener {
             login()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_GOOGLE_SING_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val account = task.getResult(ApiException::class.java)
+            Log.d(TAG,"onActivityResult: ${account?.id}")
+            val credential = GoogleAuthProvider.getCredential(account?.idToken,null)
+            FirebaseAuth.getInstance()
+                .signInWithCredential(credential)
+                .addOnCompleteListener { task ->
+                   if (task.isSuccessful){
+                       setResult(Activity.RESULT_OK)
+                       finish()
+                   }else{
+                       Log.d(TAG,"onActivityResult: ${task.exception?.message}")
+                       Snackbar.make(main_signin, "Firebase authentication faild" , Snackbar.LENGTH_LONG).show()
+                   }
+                }
+
         }
     }
 
@@ -42,7 +84,6 @@ class SinginActivity : AppCompatActivity() {
                 }
             }
     }
-
     private fun signUp() {
         val sEmail = email.text.toString()
         val sPasswd = password.text.toString()
